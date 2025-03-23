c2ec931d4a3c175a5b2da3069926437597c75085
kitlee0 <1150080022@sv.hcmunre.edu.vn>
2025-03-23 10:20:54 +0700

    oke

--- bakery_backend/cakeshop/urls.py
+++ bakery_backend/cakeshop/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 from .views import cake_list
 from django.conf import settings
 from .views import custom_login
+from .views import register
 
 
 urlpatterns = [
@@ -12,4 +13,5 @@ urlpatterns = [
     path('cakes/<int:cake_id>/', cake_detail, name='cake_detail'),
     path('cakes/', cake_list, name='cake_list'),
     path('login/', custom_login, name='login'),
+    path('register/', register, name='register')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- bakery_backend/cakeshop/views.py
+++ bakery_backend/cakeshop/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from .models import Cake
 from pymongo import MongoClient
 from bson import ObjectId
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from .forms import UserRegistrationForm
 from django.conf import settings
 from .models import cakes_collection, categories_collection  # Import collection từ models
 client = MongoClient('mongodb://localhost:27017/')  # Your MongoDB connection string
@@ -45,3 +50,16 @@ def custom_login(request):
 def cake_detail(request, cake_id):
     cake = get_object_or_404(Cake, id=cake_id)
     return render(request, 'cakeshop/cake_detail.html', {'cake': cake})
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            messages.success(request, "Đăng ký thành công! Hãy đăng nhập.")
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    
+    return render(request, 'cakeshop/register.html', {'form': form})
